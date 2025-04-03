118c99a93f0b0d735853e3201c2bf593822d2679
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2025-04-03 11:27:32 -0400

    update api

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const fastifyFormbody = require('@fastify/formbody');
 const fastifyCookie = require('@fastify/cookie');
 
 fastify.register(fastifyCors, {
-    origin: ['http://localhost:3000', 'https://test-cookie-iota.vercel.app'],
+    origin: ['http://localhost:5173', 'https://test-cookie-iota.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
@@ -32,11 +32,12 @@ fastify.get('/api/set-cookie', async (req, reply) => {
     reply
         .setCookie('token', '123456', {
             httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
-            sameSite: "None",
+            // secure: process.env.NODE_ENV !== 'development',
+            secure: true,
+            // sameSite: "None",
             path: '/',
             maxAge: 24 * 60 * 60 * 1000,
-            domain: ".vercel.app"
+            // domain: ".vercel.app"
         })
         .send({ message: 'Cookie set successfully' });
 });
