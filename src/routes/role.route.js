75a9812d53ecccddf23a301df0591acf7f6d30b5
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2025-04-09 15:11:53 +0000

    update role routes

--- src/routes/role.route.js
+++ src/routes/role.route.js
@@ -4,7 +4,7 @@ async function roleRoutes(fastify) {
     fastify.post('/roles', roleController.createRole);
     fastify.get('/roles', roleController.getAllRoles);
     fastify.get('/roles/:id', roleController.getRoleById);
-    fastify.get('/roles/:id/permissions', roleController.getRoleById);
+    fastify.get('/roles/:id/permissions', roleController.getPermissionsOfRole);
     fastify.put('/roles/:id', roleController.updateRoleById);
     fastify.delete('/roles/:id', roleController.deleteRoleById);
     
