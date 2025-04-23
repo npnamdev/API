a18802fde3dceb794b740b9cb80bc8c260fedf7b
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2025-04-23 20:20:49 +0700

    Update cors.js

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-    origin: ['http://localhost:3000', 'https://test-cookie-iota.vercel.app', "https://app.wedly.info", "https://wedly.info", "https://glorious-meme-9vqgp74p6jv3p74r-3000.app.github.dev", "https://frontify-gray.vercel.app"],
+    origin: ['http://localhost:3000', 'https://test-cookie-iota.vercel.app',"https://crispy-space-invention-7qrjgv4gxv5hrqvg-3000.app.github.dev", "https://app.wedly.info", "https://wedly.info", "https://glorious-meme-9vqgp74p6jv3p74r-3000.app.github.dev", "https://frontify-gray.vercel.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
