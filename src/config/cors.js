49f023d10b8c8bdc7517ab9466fc015cb6398dfe
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2025-05-11 06:50:32 +0000

    update 2

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-    origin: ['http://localhost:3000',"https://frame.wedly.info", 'https://test-cookie-iota.vercel.app',"https://crispy-space-invention-7qrjgv4gxv5hrqvg-3000.app.github.dev", "https://app.wedly.info", "https://wedly.info", "https://glorious-meme-9vqgp74p6jv3p74r-3000.app.github.dev", "https://frontify-gray.vercel.app", "https://quizify.wedly.info"],
+    origin: ['http://localhost:3000',"https://frame.wedly.info", 'https://test-cookie-iota.vercel.app',"https://crispy-space-invention-7qrjgv4gxv5hrqvg-3000.app.github.dev", "https://app.wedly.info", "https://wedly.info", "https://glorious-meme-9vqgp74p6jv3p74r-3000.app.github.dev", "https://frontify-gray.vercel.app", "https://quizify.wedly.info", "*", "https://scaling-space-journey-pvg5xjqx99j25vx-3000.app.github.dev"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
