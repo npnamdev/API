4b22e3a4dba12167690adccfb0f91ff95a4d3f8d
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2025-05-18 17:11:32 +0000

    update

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-    origin: ['http://localhost:3000', "https://wedly.info", "https://quizify.wedly.info", "https://refactored-space-cod-x69gj5qj9qphp6gx.github.dev"],
+    origin: ['http://localhost:3000', "https://wedly.info", "https://quizify.wedly.info", "https://refactored-space-cod-x69gj5qj9qphp6gx-3000.app.github.dev"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
