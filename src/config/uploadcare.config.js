0ea1feb73d9c4511b438a4b56a89e135c3c0e239
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2025-05-18 08:09:10 +0000

    fixbug

--- src/config/uploadcare.config.js
+++ src/config/uploadcare.config.js
@@ -1,8 +1,8 @@
-const { UploadcareClient } = require('@uploadcare/rest-client');
+const { UploadClient } = require('@uploadcare/upload-client');
 
-const uploadcare = new UploadcareClient({
+const uploadcare = new UploadClient({
     publicKey: '7a205f34fa9762f1dbb0',
     secretKey: 'ab8f936f3c659f61604e',
-});
+})
 
 module.exports = uploadcare;
