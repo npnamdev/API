3c461f66629863d0737d88cbaa33d65f57011924
NamDev <npnam031101@gmail.com>
2025-05-23 10:48:01 +0700

    update

--- src/routes/course.route.js
+++ src/routes/course.route.js
@@ -4,7 +4,7 @@ async function courseRoutes(fastify, options) {
     fastify.get('/courses', courseController.getAllCourses);
     fastify.get('/courses/:id', courseController.getCourseById);
     fastify.post('/courses', courseController.createCourse);
-    fastify.post('/api/courses/bulk', courseController.createManyCourses);
+    fastify.post('/courses/bulk', courseController.createManyCourses);
     fastify.put('/courses/:id', courseController.updateCourse);
     fastify.delete('/courses/:id', courseController.deleteCourse);
 }
