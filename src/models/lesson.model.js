ccc32ff065437bb760d17e2d4e74c2f40517cd8e
NamDev <npnam031101@gmail.com>
2025-05-28 15:39:47 +0700

    update

--- src/models/lesson.model.js
+++ src/models/lesson.model.js
@@ -6,7 +6,6 @@ const lessonSchema = new mongoose.Schema({
   content: String,
   videoUrl: String,
   duration: Number,
-  course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
   order: Number,
   isPreview: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now }
