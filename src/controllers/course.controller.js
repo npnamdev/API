072e0b666571d046407a01f97ebfeba5ff31f821
NamDev <npnam031101@gmail.com>
2025-05-29 16:25:28 +0700

    update

--- src/controllers/course.controller.js
+++ src/controllers/course.controller.js
@@ -2,22 +2,59 @@ const Course = require('../models/course.model');
 const Chapter = require('../models/chapter.model');
 const Lesson = require('../models/lesson.model');
 
+// exports.getCourseFullDetail = async (req, reply) => {
+//     try {
+//         const courseId = req.params.id;
+
+//         const course = await Course.findById(courseId);
+//         if (!course) return reply.code(404).send({ message: 'Course not found' });
+
+//         const chapters = await Chapter.find({ courseId: courseId });
+//         const chapterIds = chapters.map(ch => ch._id);
+
+//         const lessons = await Lesson.find({ chapterId: { $in: chapterIds } });
+//         const fullChapters = chapters.map(ch => ({
+//             ...ch.toObject(),
+//             lessons: lessons.filter(lesson => lesson.chapterId.toString() === ch._id.toString())
+//         }));
+
+//         return reply.send({
+//             ...course.toObject(),
+//             chapters: fullChapters
+//         });
+
+//     } catch (error) {
+//         console.error(error);
+//         return reply.code(500).send({ message: 'Internal server error' });
+//     }
+// };
+
 exports.getCourseFullDetail = async (req, reply) => {
     try {
         const courseId = req.params.id;
 
         const course = await Course.findById(courseId);
-        if (!course) return reply.code(404).send({ message: 'Course not found' });
+        if (!course) {
+            return reply.code(404).send({ message: 'Course not found' });
+        }
 
-        const chapters = await Chapter.find({ courseId: courseId });
+        // Lấy danh sách chương, sắp xếp theo 'order'
+        const chapters = await Chapter.find({ courseId }).sort({ order: 1 });
+
+        // Lấy ID các chương
         const chapterIds = chapters.map(ch => ch._id);
 
-        const lessons = await Lesson.find({ chapterId: { $in: chapterIds } });
+        // Lấy danh sách bài học, sắp xếp theo 'order'
+        const lessons = await Lesson.find({ chapterId: { $in: chapterIds } }).sort({ order: 1 });
+
+        // Gộp bài học vào từng chương
         const fullChapters = chapters.map(ch => ({
             ...ch.toObject(),
-            lessons: lessons.filter(lesson => lesson.chapterId.toString() === ch._id.toString())
+            lessons: lessons
+                .filter(lesson => lesson.chapterId.toString() === ch._id.toString())
         }));
 
+        // Trả về dữ liệu đầy đủ
         return reply.send({
             ...course.toObject(),
             chapters: fullChapters
@@ -28,7 +65,6 @@ exports.getCourseFullDetail = async (req, reply) => {
         return reply.code(500).send({ message: 'Internal server error' });
     }
 };
-
 
 exports.getAllCourses = async (request, reply) => {
     try {
