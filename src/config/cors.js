11bfac26759402f43f07b20b304123f781f4ec45
namdev311 <npnam311@gmail.com>
2025-06-24 12:39:29 +0000

    update

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,5 +1,10 @@
 const corsOptions = {
-    origin: ['http://localhost:3000', "https://wedly.info", "https://quizify.wedly.info", "https://refactored-space-cod-x69gj5qj9qphp6gx-3000.app.github.dev"],
+    origin: [
+        'http://localhost:3000', 
+        "https://wedly.info", 
+        "https://refactored-space-cod-x69gj5qj9qphp6gx-3000.app.github.dev",
+        "https://psychic-space-giggle-5g95w4664rvj279jq-3000.app.github.dev"
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
