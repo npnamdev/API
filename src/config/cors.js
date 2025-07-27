74db4a7e5df13e168deeea3706081d2a7474d198
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2025-07-27 15:29:37 +0000

    update cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,9 +1,8 @@
 const corsOptions = {
     origin: [
-        'http://localhost:3000', 
-        "https://wedly.info", 
-        "https://refactored-space-cod-x69gj5qj9qphp6gx-3000.app.github.dev",
-        "https://psychic-space-giggle-5g95w4664rvj279jq-3000.app.github.dev"
+        'http://localhost:3000',
+        "https://wedly.info",
+        "https://didactic-funicular-x55q7vw6q56c997q-3000.app.github.dev",
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
